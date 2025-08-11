a4e25a3413c1398f78a6bfccb0a2a4e49eaf859a
milad <m.geravand@asax.ir>
2025-08-11 17:12:04 +0330

    feat: add home controller

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace MyApiProject.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")] // route will be /home
+
+
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public int Index(){
+
+            int sum = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                sum += i;
+            }
+            return sum; 
+        }
+    }
+}
