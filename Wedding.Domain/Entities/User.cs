4b97e0d29d0c87e7e99f9d3a6c7b67e9a58e3963
milad <m.geravand@asax.ir>
2025-08-18 10:50:51 +0330

    feat: make structre

---
+++ Wedding.Domain/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+
+    // Relation: Posts
+    public List<Post> Posts { get; set; } = new();
+
+    // Relation: Comments
+    public List<Comment> Comments { get; set; } = new();
+}
